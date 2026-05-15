5bd532fc3d6f1f1300a6c83b0754bbefa2004a38
AdariHanish <honeyhanish786@gmail.com>
2026-05-16 03:14:34 +0530

    db

--- api/db.ts
+++ api/db.ts
@@ -4,7 +4,7 @@ const dbConfig = {
   host: process.env.TIDB_HOST || 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com',
   port: parseInt(process.env.TIDB_PORT || '4000'),
   user: process.env.TIDB_USER || '2r32GhnXE46aPEJ.root',
-  password: process.env.TIDB_PASSWORD || '2R4qAiCxEDIvPSjZ',
+  password: process.env.TIDB_PASSWORD || 'c6JJgBGmkI6pYkWR',
   database: process.env.TIDB_DATABASE || 'test',
   ssl: {
     // Try with rejectUnauthorized: false if true fails
